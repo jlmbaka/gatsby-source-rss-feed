d60f8bfb0506ab707c37977a4f9405a88f85f17a
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2022-09-04 15:23:46 +0100

    Update it to specifically take into account goodread use case

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,8 +1,9 @@
-import crypto from 'crypto'
-import Parser from 'rss-parser'
-import omitBy from 'lodash/omitBy'
+import crypto from "crypto"
+import Parser from "rss-parser"
+import omitBy from "lodash/omitBy"
+import { createRemoteFileNode } from "gatsby-source-filesystem"
 
-const normalize = (item) => {
+const normalize = item => {
   const namespaceMatched = Object.keys(item).filter(e => e.match(/:/))
   if (namespaceMatched.length === 0) {
     return item
@@ -24,13 +25,13 @@ const normalize = (item) => {
 }
 
 const renameSymbolMap = {
-  _: 'text',
-  $: 'attrs',
+  _: "text",
+  $: "attrs",
 }
 
-const renameSymbolKeys = (obj) => {
+const renameSymbolKeys = obj => {
   Object.keys(obj).forEach(key => {
-    if (typeof　obj[key] === 'object') {
+    if (typeof obj[key] === "object") {
       renameSymbolKeys(obj[key])
     }
     if (renameSymbolMap[key]) {
@@ -40,39 +41,33 @@ const renameSymbolKeys = (obj) => {
   })
 }
 
-
 const createContentDigest = obj =>
-  crypto
-    .createHash(`md5`)
-    .update(JSON.stringify(obj))
-    .digest(`hex`)
+  crypto.createHash(`md5`).update(JSON.stringify(obj)).digest(`hex`)
 
-exports.sourceNodes = async ({
-  actions,
-  createNodeId
-}, {
-  url,
-  name,
-  parserOption = {}
-}) => {
+exports.sourceNodes = async (
+  { actions, createNodeId },
+  { url, name, parserOption = {} }
+) => {
   if (!url) {
-    throw new Error('url is required.')
+    throw new Error("url is required.")
   }
 
   if (!name) {
-    throw new Error('name is required.')
+    throw new Error("name is required.")
   }
 
   const { createNode } = actions
-  const parser = new Parser(parserOption)
 
+  const parser = new Parser(parserOption)
   const feed = await parser.parseURL(url)
   const { items, ...other } = feed
 
-  items.forEach(item => {
+  items.forEach(async item => {
     const nodeId = createNodeId(item.guid || item.link)
     const normalizedItem = normalize(item)
     renameSymbolKeys(normalizedItem)
+
+    // Create Node
     createNode({
       ...normalizedItem,
       id: nodeId,
@@ -80,21 +75,52 @@ exports.sourceNodes = async ({
       children: [],
       internal: {
         contentDigest: createContentDigest(item),
-        type: `Feed${name}`
-      }
+        type: `Feed${name}`,
+      },
     })
   })
 
-  const meta = {};
-  Object.keys(other).forEach(key => meta[key] = feed[key]);
+  const meta = {}
+  Object.keys(other).forEach(key => (meta[key] = feed[key]))
   createNode({
-    id: createNodeId(name),
+    id: createNodeId(`Feed${name}`),
     ...meta,
     parent: null,
     children: [],
     internal: {
       contentDigest: createContentDigest(feed.title),
-      type: `Feed${name}Meta`
+      type: `Feed${name}Meta`,
+    },
+  })
+}
+
+// called each time a node is created
+exports.onCreateNode = async ({
+  node, // the node that was just created
+  actions: { createNode, createNodeField },
+  createNodeId,
+  getCache,
+}) => {
+  if (node.internal.type === "FeedGoodreadsBook") {
+    const fileNode = await createRemoteFileNode({
+      // the url of the remote image to generate a node for
+      url: node.coverImageUrl,
+      parentNodeId: node.id,
+      createNode,
+      createNodeId,
+      getCache,
+    })
+    if (fileNode) {
+      createNodeField({ node, name: "coverImage", value: fileNode.id })
     }
-  });
+  }
+}
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  createTypes(`
+    type FeedGoodreadsBook implements Node {
+      coverImage: File @link(from: "fields.coverImage")
+    }
+  `)
 }
